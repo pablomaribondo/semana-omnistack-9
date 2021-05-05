e961dbb03021c7ea9f8ca63832cb072e7f8f9065
Pablo Maribondo <pablomgvieira@gmail.com>
2021-05-05 11:06:05 -0300

    (server) cadastro de spot implementado

--- server/src/controllers/SpotController.js
+++ server/src/controllers/SpotController.js
@@ -1,5 +1,26 @@
+const Spot = require("../models/Spot");
+const User = require("../models/User");
+
 module.exports = {
   async store(request, response) {
-    return response.json({ ok: true });
+    const { filename } = request.file;
+    const { company, techs, price } = request.body;
+    const { user_id } = request.headers;
+
+    const user = await User.findById(user_id);
+
+    if (!user) {
+      return response.status(400).json({ error: "User does not exitsts." });
+    }
+
+    const spot = await Spot.create({
+      user: user_id,
+      thumbnail: filename,
+      company,
+      techs: techs.split(",").map(tech => tech.trim()),
+      price,
+    });
+
+    return response.json(spot);
   },
 };
