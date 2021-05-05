5fba3704215dd6c0e114dfac23a0d1f3f97d7d5c
Pablo Maribondo <pablomgvieira@gmail.com>
2021-05-05 11:26:23 -0300

    (server) reserva de spots implementada

---
+++ server/src/controllers/BookingController.js
@@ -0,0 +1,20 @@
+const Booking = require("../models/Booking");
+const { store } = require("./SpotController");
+
+module.exports = {
+  async store(request, response) {
+    const { user_id } = request.headers;
+    const { spot_id } = request.params;
+    const { date } = request.body;
+
+    const booking = await Booking.create({
+      user: user_id,
+      spot: spot_id,
+      date,
+    });
+
+    await booking.populate("spot").populate("user").execPopulate();
+
+    return response.json(booking);
+  },
+};
--- server/src/routes.js
+++ server/src/routes.js
@@ -3,6 +3,7 @@ const multer = require("multer");
 
 const uploadConfig = require("./config/upload");
 
+const BookingController = require("./controllers/BookingController");
 const DashboardController = require("./controllers/DashboardController");
 const SessionController = require("./controllers/SessionController");
 const SpotController = require("./controllers/SpotController");
@@ -16,5 +17,7 @@ routes.get("/spots", SpotController.index);
 routes.post("/spots", upload.single("thumbnail"), SpotController.store);
 
 routes.get("/dashboard", DashboardController.show);
+
+routes.post("/spots/:spot_id/bookings", BookingController.store);
 
 module.exports = routes;
