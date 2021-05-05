43600d0e6613aa0a95782ccf2e3d5317ce9b4bc3
Pablo Maribondo <pablomgvieira@gmail.com>
2021-05-05 11:15:42 -0300

    (server) listagem de spots implementado por user

--- server/src/routes.js
+++ server/src/routes.js
@@ -3,6 +3,7 @@ const multer = require("multer");
 
 const uploadConfig = require("./config/upload");
 
+const DashboardController = require("./controllers/DashboardController");
 const SessionController = require("./controllers/SessionController");
 const SpotController = require("./controllers/SpotController");
 
@@ -13,5 +14,7 @@ routes.post("/sessions", SessionController.store);
 
 routes.get("/spots", SpotController.index);
 routes.post("/spots", upload.single("thumbnail"), SpotController.store);
+
+routes.get("/dashboard", DashboardController.show);
 
 module.exports = routes;
