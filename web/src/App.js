668fba9c8b86a8a177f9d3822829bdc1e989b0a2
Pablo Maribondo <pablomgvieira@gmail.com>
2021-05-13 14:04:01 -0300

    (web) layout do login implementado

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,27 @@
 import "./App.css";
 
+import logo from "./assets/logo.svg";
+
 const App = () => {
   return (
-    <div>
-      <p>Hello</p>
+    <div className="container">
+      <img src={logo} alt="AirCnC" />
+
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e econtre{" "}
+          <strong>talentos</strong> para a sua empresa
+        </p>
+
+        <form>
+          <label htmlFor="email">E-MAIL *</label>
+          <input id="email" type="email" placeholder="Seu melhor e-mail" />
+
+          <button className="btn" type="submit">
+            Entrar
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
