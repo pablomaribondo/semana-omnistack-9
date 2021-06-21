3e5280216902d574bd77f7e3e18591e7eb0b0274
Pablo Maribondo <pablomgvieira@gmail.com>
2021-06-21 09:36:01 -0300

    feat(web): create dashboard component

--- web/src/pages/Dashboard/index.js
+++ web/src/pages/Dashboard/index.js
@@ -1,8 +1,41 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import "./styles.css";
+import api from "../../services/api";
+
 const Dashboard = () => {
+  const [spots, setSpots] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const userId = localStorage.getItem("user");
+      const response = await api.get("/dashboard", {
+        headers: { user_id: userId },
+      });
+
+      setSpots(response.data);
+    })();
+  }, []);
+
   return (
-    <div>
-      <p>Dashboard</p>
-    </div>
+    <>
+      <ul className="spot-list">
+        {spots.map(spot => (
+          <li key={spot._id}>
+            <header
+              style={{ backgroundImage: `url(${spot.thumbnail_url})` }}
+            ></header>
+            <strong>{spot.company}</strong>
+            <span>{spot.price ? `R$${spot.price}/dia` : "GRATUITO"}</span>
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/new">
+        <button className="btn">Cadastrar novo spot</button>
+      </Link>
+    </>
   );
 };
 
