d2a97c8c40341ccc441205c09d8be069bcc19f2d
Pablo Maribondo <pablomgvieira@gmail.com>
2021-06-21 09:35:35 -0300

    feat(server): add thumbnail_url field to spotSchema

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const routes = require("./routes");
@@ -16,6 +17,7 @@ mongoose.connect(MONGO_ATLAS_URI, {
 
 app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 app.listen(PORT, () => {
