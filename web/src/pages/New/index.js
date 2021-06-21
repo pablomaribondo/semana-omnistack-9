bb2193bad402f2175f3121d84a23ed6d9a2a74ab
Pablo Maribondo <pablomgvieira@gmail.com>
2021-06-21 10:03:46 -0300

    feat(web): create new spot component

--- web/src/pages/New/index.js
+++ web/src/pages/New/index.js
@@ -1,8 +1,86 @@
+import { useState, useMemo } from "react";
+import { useHistory } from "react-router-dom";
+
+import "./styles.css";
+import api from "../../services/api";
+import camera from "../../assets/camera.svg";
+
 const New = () => {
+  const [company, setCompany] = useState("");
+  const [techs, setTechs] = useState("");
+  const [price, setPrice] = useState("");
+  const [thumbnail, setThumbnail] = useState(null);
+
+  const history = useHistory();
+
+  const preview = useMemo(() => {
+    return thumbnail ? URL.createObjectURL(thumbnail) : null;
+  }, [thumbnail]);
+
+  const submitHandler = async event => {
+    event.preventDefault();
+
+    const data = new FormData();
+    const userId = localStorage.getItem("user");
+
+    data.append("thumbnail", thumbnail);
+    data.append("company", company);
+    data.append("techs", techs);
+    data.append("price", price);
+
+    await api.post("/spots", data, {
+      headers: { user_id: userId },
+    });
+
+    history.push("/dashboard");
+  };
+
   return (
-    <div>
-      <p>New</p>
-    </div>
+    <form onSubmit={submitHandler}>
+      <label
+        id="thumbnail"
+        style={{ backgroundImage: `url(${preview})` }}
+        className={thumbnail ? "has-thumbnail" : ""}
+      >
+        <input
+          type="file"
+          onChange={({ target: { files } }) => setThumbnail(files[0])}
+        />
+        <img src={camera} alt="Select Input" />
+      </label>
+
+      <label htmlFor="company">EMPRESA *</label>
+      <input
+        id="company"
+        placeholder="Sua empresa incrível"
+        value={company}
+        onChange={({ target: { value } }) => setCompany(value)}
+      />
+
+      <label htmlFor="techs">
+        TECNOLOGIAS * <span>(separadas por vírgula)</span>
+      </label>
+      <input
+        id="techs"
+        placeholder="Quais tecnologias usam?"
+        value={techs}
+        onChange={({ target: { value } }) => setTechs(value)}
+      />
+
+      <label htmlFor="techs">
+        VALOR DA DIÁRIA * <span>(em branco para GRATUITO)</span>
+      </label>
+      <input
+        id="price"
+        placeholder="Valor cobrado por dia"
+        value={price}
+        onChange={({ target: { value } }) => setPrice(value)}
+      />
+
+      <button type="submit" className="btn">
+        Cadastrar
+      </button>
+    </form>
   );
 };
 
