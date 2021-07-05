0e5f0efe6eea20a22c20d4f2767ec0aba9101533
Pablo Maribondo <pablomgvieira@gmail.com>
2021-07-05 12:55:59 -0300

    feat: create approvals & rejections routes

--- server/src/routes.js
+++ server/src/routes.js
@@ -7,6 +7,8 @@ const BookingController = require("./controllers/BookingController");
 const DashboardController = require("./controllers/DashboardController");
 const SessionController = require("./controllers/SessionController");
 const SpotController = require("./controllers/SpotController");
+const ApprovalController = require("./controllers/ApprovalController");
+const RejectionController = require("./controllers/RejectionController");
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -19,5 +21,8 @@ routes.post("/spots", upload.single("thumbnail"), SpotController.store);
 routes.get("/dashboard", DashboardController.show);
 
 routes.post("/spots/:spot_id/bookings", BookingController.store);
+
+routes.post("/bookings/:booking_id/approvals", ApprovalController.store);
+routes.post("/bookings/:booking_id/rejections", RejectionController.store);
 
 module.exports = routes;
