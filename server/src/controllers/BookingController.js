5cd442cd78b64bb68e4569c005c39116aa8759eb
Pablo Maribondo <pablomgvieira@gmail.com>
2021-07-05 12:54:49 -0300

    feat: update BookingController to use socketio

--- server/src/controllers/BookingController.js
+++ server/src/controllers/BookingController.js
@@ -1,5 +1,4 @@
 const Booking = require("../models/Booking");
-const { store } = require("./SpotController");
 
 module.exports = {
   async store(request, response) {
@@ -14,6 +13,12 @@ module.exports = {
     });
 
     await booking.populate("spot").populate("user").execPopulate();
+
+    const ownerSocket = request.connectedUsers[booking.spot.user];
+
+    if (ownerSocket) {
+      request.io.to(ownerSocket).emit("booking_request", booking);
+    }
 
     return response.json(booking);
   },
